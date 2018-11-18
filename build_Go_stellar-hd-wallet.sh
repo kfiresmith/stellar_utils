e63cae7468a01dd78888bd8b8ea7081d57b75fef
Kodiak Firesmith <kfiresmith@gmail.com>
2018-11-18 09:00:53 -0500

    Make script less distro-dependent, update deps to reflect current deps.

--- build_Go_stellar-hd-wallet.sh
+++ build_Go_stellar-hd-wallet.sh
@@ -2,11 +2,11 @@
 # Written for Fedora, dependent package names may differ...
 
 echo " .:Run Pre-Build Tests:. "
-
-rpm -q golang || needsdeps="true"
+which git || needsdeps="true"
+which go || needsdeps="true"
 
 if [ $needsdeps ]; then
-  echo "golang not installed, install that as root first!"
+  echo "dependencies missing, install git and golang as root first!"
   exit 1
 else echo "pre-build tests passed, starting build"
 fi
@@ -27,12 +27,15 @@ go get -v -u github.com/stellar/go
 go get -v -u github.com/spf13/cobra
 go get -v -u github.com/agl/ed25519
 go get -v -u github.com/spf13/pflag
-go get -v -u github.com/nullstyle/go-xdr
 go get -v -u github.com/getsentry/raven-go
 go get -v -u github.com/certifi/gocertifi
 go get -v -u github.com/bartekn/go-bip39
 go get -v -u github.com/pkg/errors
 go get -v -u golang.org/x/crypto/pbkdf2
+go get -v -u github.com/lib/pq
+go get -v -u github.com/stellar/go-xdr/xdr3
+go get -v -u github.com/tyler-smith/go-bip39
+
 go build -o ~/bin/stellar-hd-wallet ~/go/src/github.com/stellar/go/tools/stellar-hd-wallet/main.go
 
 chmod +x ~/bin/stellar-hd-wallet
